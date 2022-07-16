6f5ced72e7b3e17add272a00137148fd6e8d876b
Joshua Dunaway <joshuadunaway23@gmail.com>
2022-07-16 14:08:16 +0200

    Built the city search component.

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8ee3bf800emsh74145b76ac2439bp106684jsnd17e8c3f90bc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
